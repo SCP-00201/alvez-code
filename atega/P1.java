11877dd07b82686f1b2602f939f70760a44659e8
SCP-00201 <ategayves@gmail.com>
2025-12-04 09:14:07 +0800

    Update and rename ACTIVITY.java to P1.java

--- atega/P1.java
+++ atega/P1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ACTIVITY{
+public class P1 {
     public static void main (String []args) {
         Scanner scanner = new Scanner (System.in);
         int number;
